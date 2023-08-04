d9a85c9f48de17ccb3b9ff4248e1eb0f02b7d9f3
ciprian <ciprian.hurmuzache@holisticai.com>
2023-08-04 13:07:50 +0100

    updated repo

--- main.py
+++ main.py
@@ -2,20 +2,5 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from exercise1.solution_exercise1 import extract_mean_1
 from data.data import data
 
-
-dict = {
-    'test': 123,
-    'test1': "sadfasdas"
-}
-
-l = [
-                    {"name": "mean", "value": 0.65715},
-                    {"name": "median", "value": 0.6575},
-                    {"name": "q05", "value": 0.624875},
-                    {"name": "q95", "value": 0.695125},
-                    {"name": "std", "value": 0.021576209775649408},
-                    {"name": "mad", "value": 0.017607000000000008},
-                ]
